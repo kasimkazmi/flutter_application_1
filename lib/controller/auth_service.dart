486fefc9cdd8402c6bcc294cfde38122cbb3eed8
Kasim Kazmi <kasimkazmi27@gmail.com>
2025-01-16 20:23:59 -0500

    Add Firebase authentication service and configuration files

---
+++ lib/controller/auth_service.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//   Sign in with email and password
+
+  Future<User?> signInWithEmail(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return userCredential.user;
+    } catch (e) {
+      print("Error signin in with email$e");
+      return null;
+    }
+  }
+
+//   Register with email and password
+
+  Future<User?> registerWithEmail(String email, String password) async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      print("Error register:$e");
+      return null;
+    }
+  }
+
+// Verify phone number
+
+  Future<void> verifyPhoneNumber(
+      String phoneNumber, Function(String) onCodeSent) async {
+    await _auth.verifyPhoneNumber(
+        phoneNumber: phoneNumber,
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          await _auth.signInWithCredential(credential);
+        },
+        verificationFailed: (FirebaseAuthException e) {
+          print("Verification failed:${e.message}");
+        },
+        codeSent: (String verificationId, int? resendToken) {
+          onCodeSent(verificationId); // Callback to handle code sent
+        },
+        codeAutoRetrievalTimeout: (String verificationId) {});
+  }
+
+// Sign in with phone number
+
+  Future<User?> signInWithPhoneNumber(
+      String verificationId, String smsCode) async {
+    try {
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(
+          verificationId: verificationId, smsCode: smsCode);
+    } catch (e) {
+      print("Error signing in with phone:$e");
+      return null;
+    }
+  }
+}
